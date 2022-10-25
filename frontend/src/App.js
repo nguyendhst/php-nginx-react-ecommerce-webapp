9a16f7fb55c8367e2a90e7285b9aa2d98f722667
nguyendhst <mountngubinh@gmail.com>
2022-10-25 23:02:46 +0700

    [frontend] added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,23 @@
-import {
-  Route,
-  Routes
-} from "react-router-dom";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-import LandingPage from './pages/LandingPage/LandingPage';
-import ProductPage from './pages/ProductPage/ProductPage';
+import NavBar from "./components/NavBar/NavBar";
+
+import LandingPage from "./pages/LandingPage/LandingPage";
+import ProductPage from "./pages/ProductPage/ProductPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/product" element={<ProductPage />} />
-    </Routes>
+    <React.Fragment>
+      <NavBar />
+
+      <main>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/product" element={<ProductPage />} />
+        </Routes>
+      </main>
+    </React.Fragment>
   );
 }
 
